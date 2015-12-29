e45dfdbe3ec528d29394834093e4d11d1924102f
Timothy Sullivan <tsullivan@elastic.co>
2015-12-29 15:21:37 -0700

    some error checking

--- lib.js
+++ lib.js
@@ -1,22 +1,34 @@
-/*eslint-disable no-console,no-unused-vars*/
-/*global process,console*/
-import fs from 'q-io/fs';
+/*eslint-disable no-console*/
+import _ from 'lodash';
 
-export function lister () {
-  const [ node, script, ...paths ] = process.argv;
-  if (!paths.length) {
-    console.log('no paths@@!!!!!');
-    return;
+export class Renamer {
+  constructor ({fs, randomer}) {
+    this.fs = fs;
+    this.randomer = randomer;
   }
 
-  Promise.all(
-    paths.map((p) => fs.list(p))
-  )
-  .then((results) => {
-    console.log(results);
-    return 'Done';
-  })
-  .then((result) => {
-    console.log(result);
-  });
+  rename (files) {
+    if (!files.length) { // check arguments
+      console.log('File paths must be given as arguments');
+      return;
+    }
+    // check they are files
+    Promise.all( files.map(p => this.fs.isFile(p)) )
+    .then((allAreFiles) => {
+      if (_.contains(allAreFiles, false)) {
+        throw new Error('All arguments must be files!');
+      }
+      return files;
+    })
+    .then((files) => {
+      // rename the files
+      Promise.all(
+        files.map((p) => this.fs.list(p))
+      )
+    })
+    .catch((error) => {
+      // something went wrong
+      console.log(error.message)
+    });
+  }
 }
